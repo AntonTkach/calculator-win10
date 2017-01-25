e6c7c82ac1c578af9ec024ec5e726743cdb3b8f7
Anton Tkach <dekerter@gmail.com>
2017-01-25 23:58:38 +0200

    Added blank test

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -5,14 +5,15 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CalculatorWin10")]
+[assembly: AssemblyTitle("Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalculatorWin10")]
+[assembly: AssemblyProduct("Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform","UAP")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ Tests/UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Tests
+{
+    [TestClass]
+    public class MathControlsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    [TestClass]
+    public class DisplayInfoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+    [TestClass]
+    public class ClickHandlerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+    [TestClass]
+    public class MemoryHandlerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
